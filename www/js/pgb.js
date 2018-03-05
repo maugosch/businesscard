97151a964aa4c64edfa7d4ac52e886d975558c8b
s-115-19 <s-115-19@gumiforest.ae.krakow.pl>
2018-03-05 17:19:38 +0100

    first commit

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -6,21 +6,20 @@ function onDeviceReady() {
 	navigator.notification.beep(1);
 }
 
-function bCard() {
-	
-	var personalInfo = {
-		
-		name: "Malgorzata",
-		surname: "Kindrat",
-		email: "yolo@swag.com",
-		website: "emo.com",
-		dean: "KrDUIs1011",
-		field: "Applied Informatics - master"
-	};
-	
-	navigator.notification.alert("Name and surname: " + personalInfo.name 
-	+ personalInfo.surname + "\n Email: " + personalInfo.email + "\n Website: " 
-	+ personalInfo.website + "\n Dean\'s group number: " + personalInfo.dean 
-	+ "\n Field of study: " + personalInfo.field);
-	
+function checkConnection() {
+    var networkState = navigator.connection.type;
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+ 
+    alert('Connection type: ' + states[networkState]);
 }
+ 
+checkConnection();
